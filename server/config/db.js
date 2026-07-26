a0b1a0cd6af84f58315f6567c7b67c4789293b89
Tharun-098 <tharun211388@gmail.com>
2026-07-26 19:25:55 +0530

    dberror

--- server/config/db.js
+++ server/config/db.js
@@ -5,10 +5,17 @@ import mongoose from "mongoose";
 const connectDatabase=async()=>{
     try{
         mongoose.connection.on('connected',()=>console.log("Database connected"))
+        mongoose.connection.on("disconnected", () => {
+            console.log("❌ MongoDB disconnected");
+        });
+
+        mongoose.connection.on("error", (err) => {
+            console.log("MongoDB Error:", err);
+        });
         //const url='mongodb+srv://tharun:tharun123@cluster0.gjhs5os.mongodb.net/realtimechat?retryWrites=true&w=majority&appName=Cluster0';
-        await mongoose.connect(process.env.MONGODB_URL)
-        console.log("Connected DB:", mongoose.connection.name);
         console.log("🔍 Connecting to:", process.env.MONGODB_URL);
+        console.log("Connected DB:", mongoose.connection.name);
+        await mongoose.connect(process.env.MONGODB_URL)
     }
     catch(error){
         console.log(error.message)
